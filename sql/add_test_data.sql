00c794dc3f378e829fef43c2ca0a102bdc51d780
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-03-23 13:25:51 +0200

    Lisätty testidataa

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Game (gamename, published_year, publisher, description, added) VALUES ('Monopoli', 1950, 'Monopoli-ukko', 'CAPITALISM HO!', NOW());
+INSERT INTO Game (gamename, published_year, publisher, description, added) VALUES ('Scythe', 2016, 'Stonemaier Games', 'Hassu satusota', NOW());
+INSERT INTO ACCOUNT (accountname, password, is_admin) VALUES ('Ville', 'salainensana', TRUE);
