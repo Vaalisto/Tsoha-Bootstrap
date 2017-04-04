1ddd7f8eba1e4159a1e582eed28017bea5599ac9
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-04-04 19:27:16 +0300

    Tehty nimen validointi base-luokkaan

--- lib/base_model.php
+++ lib/base_model.php
@@ -15,12 +15,28 @@
       }
     }
 
+    public function validate_string($string, $min, $max){
+      $errors = array();
+      if(!is_string($string)){
+        $errors[] = "On oltava merkkijono.";
+      }
+      if(strlen($string) < $min && $min != 0){
+        $errors[] = "Minimipituus on " . $min . " merkkiä.";
+      }
+      if(strlen($string) > $max && $max != 0){
+        $errors[] = "Maksimipituus on " . $max . " merkkiä.";
+      }
+      return $errors;
+    }
+
     public function errors(){
       // Lisätään $errors muuttujaan kaikki virheilmoitukset taulukkona
       $errors = array();
 
       foreach($this->validators as $validator){
         // Kutsu validointimetodia tässä ja lisää sen palauttamat virheet errors-taulukkoon
+        $nexterrors = $this->{$validator}();
+        $errors = array_merge($errors, $nexterrors);
       }
 
       return $errors;
