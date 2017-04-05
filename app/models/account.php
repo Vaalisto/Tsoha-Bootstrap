bfe692cddc17876443478ba727bc576e2a9115ee
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-04-05 14:48:01 +0300

    Unohtui tallentaa

--- app/models/account.php
+++ app/models/account.php
@@ -3,7 +3,12 @@
 		public $id, $accountname, $password, $is_admin;
 
 		public function __construct($attributes){
+			parent::__construct($attributes);
+			$this->validators = array('validate_name');
+		}
 
+		public function validate_name(){
+			
 		}
 
 	}
