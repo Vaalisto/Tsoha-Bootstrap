435057df341a1ccf7587c9e2f9084ae09a4c59df
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-04-06 12:14:21 +0300

    Korjattu basecontrollerista virhe

--- lib/base_controller.php
+++ lib/base_controller.php
@@ -5,7 +5,7 @@
     public static function get_user_logged_in(){
       if(isset($_SESSION['user'])){
         $user_id = $_SESSION['user'];
-        $user = User::find($user_id);
+        $user = Account::find($user_id);
 
         return $user;
       }
